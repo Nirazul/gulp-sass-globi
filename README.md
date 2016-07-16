b1fc748de772ab59b44b5bafd9c9278a095c3d4d
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2016-07-16 17:19:06 +0200

    Add basic files

---
+++ README.md
@@ -0,0 +1,53 @@
+# About gulp-sass-globi
+
+This started off as a fork of [gulp-sass-glob](https://www.npmjs.com/package/gulp-sass-glob).
+As some long-standing bugs and feature requests haven't been addressed, I decided to fork the repository and try to develop the plugin further.
+
+# Overview
+
+[Gulp](http://gulpjs.com/) plugin for [gulp-sass](https://github.com/dlmanning/gulp-sass) to use glob imports.
+
+# Install
+
+```
+npm install gulp-sass-glob --save-dev
+```
+
+# Basic Usage
+
+main.scss
+
+```scss
+@import "vars/**/*.scss";
+@import "mixins/**/*.scss";
+@import "generic/**/*.scss";
+@import "../components/**/*.scss";
+@import "../views/**/*.scss";
+@import "../views/**/*something.scss";
+@import "../views/**/all.scss";
+```
+
+*NOTE*: Also support using `'` (single quotes) for example: `@import 'vars/**/*.scss';`
+
+gulpfile.js
+
+```javascript
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+
+gulp.task('styles', function () {
+    return gulp
+        .src('src/styles/main.scss')
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(gulp.dest('dist/styles'));
+});
+```
+
+# Troubleshooting
+
+# Planned features
+
+- Add support for nested globbing
+- Add support for [includePaths](https://github.com/sass/node-sass#includepaths)
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import path from 'path'
+import fs from 'fs'
+import through from 'through2'
+import glob from 'glob'
+import slash from 'slash'
+
+export default function gulpSassGlob () {
+  return through.obj(transform)
+}
+
+function transform (file, env, callback) {
+  const reg = /^\s*@import\s+["']([^"']+\*[^"']*(\.scss|\.sass)?)["'];?$/gm
+  const isSass = path.extname(file.path) === '.sass'
+  const base = path.normalize(path.join(path.dirname(file.path), '/'))
+
+  let contents = file.contents.toString('utf-8')
+  let contentsCount = contents.split('\n').length
+
+  let result
+
+  for(var i=0; i<contentsCount; i++) {
+    result = reg.exec(contents)
+
+    if(result !== null) {
+      const importRule = result[0]
+      const globPattern = result[1]
+
+      const files = glob.sync(path.join(base, globPattern), {
+        cwd: base
+      })
+
+      let imports = []
+
+      files.forEach((filename) => {
+        if (filename !== file.path && isSassOrScss(filename)) {
+          // remove parent base path
+          filename = path.normalize(filename).replace(base, '')
+          imports.push('@import "' + slash(filename) + '"' + (isSass ? '' : ';'))
+        }
+      })
+
+      const replaceString = imports.join('\n')
+      contents = contents.replace(importRule, replaceString)
+      file.contents = new Buffer(contents)
+    }
+  }
+
+  callback(null, file)
+}
+
+function isSassOrScss (filename) {
+  return (!fs.statSync(filename).isDirectory() && path.extname(filename).match(/\.sass|\.scss/i))
+}
